770174d13f29f0f02554db585e3aada44f09c2b1
MRX7014 | Octopus <safen894@gmail.com>
2024-08-18 23:10:44 +0300

    Update LightDebloat.sh

--- features/Debloater/LightDebloat.sh
+++ features/Debloater/LightDebloat.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 clear
 echo "Light Debloat Starting"
+echo "Do you want to continue? (y/n)"
+read -p "Choose: " user_debloat
+if [ $user_debloat == y ]
+then
 sleep 2
 sudo pm uninstall --user 0 android.autoinstalls.config.samsung
 sudo pm uninstall --user 0 com.android.bips
@@ -157,3 +161,8 @@ sudo pm uninstall --user 0 com.snap.camerakit.plugin.v1
 sleep 2
 echo "Done"
 bash AFFT.sh
+fi
+if [ $user_debloat == n ]
+then
+bash AFFT.sh
+fi
