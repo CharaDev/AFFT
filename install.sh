386c16fcfda31f6048b57abc4949c904b89dd45e
mrx7014 <safen894@gmail.com>
2025-03-26 17:22:16 +0200

    V9.1.1

--- install.sh
+++ install.sh
@@ -130,71 +130,6 @@ ui_print "🎮 Activating Vulkan Performance Tweaks for Smoother Gaming"
 sleep 1
 ui_print "✅ AFFT Enhancements Applied Successfully!"
 sleep 2
-ui_print ""
-ui_print "Now Start DT tweaks"
-sleep 3
-ui_print ""
-ui_print "*******************************"
-ui_print "*      DT - Device Tweaker    *"
-ui_print "* Applying Performance Tweaks *"
-ui_print "*******************************"
-sleep 1
-ui_print "- Optimizing boot time... 🚀"
-sleep 1
-ui_print "- Enhancing responsiveness & speed... ⚡"
-sleep 1
-ui_print "- Stabilizing FPS for smoother performance... 🎮"
-sleep 1
-ui_print "- Disabling data collection & tracking... 🔒"
-sleep 1
-ui_print "- Accelerating app launch & freeing RAM... 🏎️"
-sleep 1
-ui_print "- Extending battery life without performance drop... 🔋"
-sleep 1
-ui_print "- Switching rendering driver to Vulkan for better graphics... 🎨"
-sleep 1
-ui_print "- Improving RAM management for efficiency... 🧠"
-sleep 1
-ui_print "- Disabling unnecessary Google Mobile Services (GMS)... 📉"
-sleep 1
-ui_print "- Enabling aggressive RAM killer (Prop tweak)... 🗑️"
-sleep 1
-ui_print "- Stopping unnecessary log sending... 🚫"
-sleep 1
-ui_print "- Enabling hardware acceleration for smoother graphics... 🖥️"
-sleep 1
-ui_print "- Adding Vulkan X6739 properties... 🔧"
-sleep 1
-ui_print "- Updating Vulkan version to 1.3... 🆕"
-sleep 2
-ui_print "✅ DT Enhancements Applied Successfully!"
-sleep 2
-ui_print ""
-ui_print "- Now start YAKT script 📃"
-sleep 2
-ui_print "- Reduces Jitter and Latency"
-sleep 1
-ui_print "- Optimizes Ram Management"
-sleep 1
-ui_print "- Disables scheduler logs/stats"
-sleep 1
-ui_print "- Disables printk logs"
-sleep 1
-ui_print "- Disables SPI CRC"
-sleep 1
-ui_print "- Tweaks mglru"
-sleep 1
-ui_print "- Allows sched boosting on top-app tasks (Thx to tytydraco)"
-sleep 1
-ui_print "- Tweaks uclamp scheduler (Credits to darkhz for uclamp tweak)"
-sleep 1
-ui_print "- Sets -20 (highest priority) for the most essential processes"
-sleep 1
-ui_print "- Uses Google's schedutil rate-limits from Pixel 3"
-sleep 4
-ui_print "✅ YAKT Enhancements Applied Successfully!"
-sleep 2
-ui_print ""
 ui_print "🎉 Enjoy Your Upgraded Galaxy A23! 🎉"
 }
 
@@ -217,40 +152,6 @@ on_install()
     fi
     ui_print ""
     }
-# shellcheck disable=SC2148
-# shellcheck disable=SC2034
-SKIPUNZIP=1
-RM_RF() {
-rm /sdcard/Documents/yakt/yakt.log 2>/dev/null
-rm /sdcard/yakt.log 2>/dev/null
-rm /sdcard/yakt/yakt.txt 2>/dev/null
-rm "${MODPATH}/yakt.log" 2>/dev/null
-rm "${MODPATH}/yakt-logging-error.log" 2>/dev/null
-rm "${MODPATH}/LICENSE" 2>/dev/null
-rm "${MODPATH}/README.md" 2>/dev/null
-}
-SET_PERMISSION() {
-ui_print "Install YAKT first to use yakt.sh"
-ui_print "- Setting YAKT Permissions"
-set_perm_recursive "$MODPATH" 0 0 0755 0644
-set_perm_recursive "${MODPATH}/yakt.sh" 0 0 0755 0700
-}
-MOD_EXTRACT() {
-ui_print "- Extracting YAKT Files"
-unzip -o "$ZIPFILE" yakt.sh -d "$MODPATH" >&2
-unzip -o "$ZIPFILE" service.sh -d "$MODPATH" >&2
-unzip -o "$ZIPFILE" module.prop -d "$MODPATH" >&2
-}
-MOD_PRINT() {
-ui_print "- YAKT"
-ui_print "- Installing"
-}
-set -x
-RM_RF
-MOD_PRINT
-MOD_EXTRACT
-SET_PERMISSION
-sleep 4
 # Only some special files require specific permissions
 # This function will be called after on_install is done
 # The default permissions should be good enough for most cases
